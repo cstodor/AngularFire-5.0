7f670fe36df3fbc4c3790c86e9f188c11d307669
Csaba Todor <csaba.todor@outlook.com>
2017-11-02 11:44:44 +0200

    Added SidebarComponent, ChipsComponent

--- src/app/components/material-design/material-design.module.ts
+++ src/app/components/material-design/material-design.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Material Design
+import { MatSidenavModule, MatIconModule, MatListModule } from "@angular/material";
+
+// Modules
+import { MaterialDesignRoutingModule } from "./material-design-routing.module";
+
+// Components
+import { MaterialDesignComponent } from "./material-design.component";
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { ChipsComponent } from './chips/chips.component';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialDesignRoutingModule,
+    MatSidenavModule, 
+    MatIconModule,
+    MatListModule
   ],
-  declarations: []
+  declarations: [
+    MaterialDesignComponent,
+    HomeComponent,
+    SidebarComponent,
+    ChipsComponent
+  ]
 })
 export class MaterialDesignModule { }
